24e67cb19f9230e2d6baad5de4cd321f7978289d
Nabyla Sharfina <nabylasharfina8@gmail.com>
2025-11-24 21:33:39 +0700

    Update 07_ETL_Procedures.sql

--- sql/07_ETL_Procedures.sql
+++ sql/07_ETL_Procedures.sql
@@ -5,28 +5,39 @@ BEGIN
 
     MERGE dbo.Dim_Mahasiswa AS target
     USING (
-        SELECT nim, nama_mahasiswa, jenis_kelamin, angkatan, status, kode_prodi
+        SELECT 
+            nim, 
+            nama_mahasiswa,
+            jenis_kelamin,
+            angkatan,
+            status,
+            kode_prodi
         FROM stg.Mahasiswa
     ) AS src
-    ON target.nim = src.nim AND target.IsCurrent = 1
+        ON target.nim = src.nim
 
-    WHEN MATCHED AND (
-        target.nama_mahasiswa <> src.nama_mahasiswa OR
-        target.jenis_kelamin <> src.jenis_kelamin OR
-        target.angkatan <> src.angkatan OR
-        target.status <> src.status OR
-        target.id_prodi <> src.kode_prodi
-    )
-    THEN UPDATE SET 
-        target.IsCurrent = 0,
-        target.ExpiryDate = GETDATE();
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.nama_mahasiswa = src.nama_mahasiswa,
+            target.jenis_kelamin = src.jenis_kelamin,
+            target.angkatan = src.angkatan,
+            target.status = src.status,
+            target.sk_prodi = (
+                SELECT sk_prodi 
+                FROM dbo.Dim_ProgramStudi 
+                WHERE id_prodi = src.kode_prodi
+            ),
+            target.updated_date = GETDATE()
 
     WHEN NOT MATCHED THEN
-        INSERT (nim, nama_mahasiswa, jenis_kelamin, angkatan, status, sk_prodi, EffectiveDate, IsCurrent)
+        INSERT (nim, nama_mahasiswa, jenis_kelamin, angkatan, status, sk_prodi)
         VALUES (
-            src.nim, src.nama_mahasiswa, src.jenis_kelamin, src.angkatan, src.status,
-            (SELECT sk_prodi FROM dbo.Dim_ProgramStudi WHERE id_prodi = src.kode_prodi),
-            GETDATE(), 1
+            src.nim,
+            src.nama_mahasiswa,
+            src.jenis_kelamin,
+            src.angkatan,
+            src.status,
+            (SELECT sk_prodi FROM dbo.Dim_ProgramStudi WHERE id_prodi = src.kode_prodi)
         );
 END;
 GO
