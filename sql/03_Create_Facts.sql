25e8bcc1b97b3c28ba8a1481369292b4d7ca91d0
AriniPuteri <arini.123450069@student.itera.ac.id>
2025-11-24 18:56:25 +0700

    File 03: Creat_Facts.sql

---
+++ sql/03_Create_Facts.sql
@@ -0,0 +1,85 @@
+-- Fact Table: Fact_Prestasi
+CREATE TABLE dbo.Fact_Prestasi (
+    sk_prestasi INT NOT NULL,
+    sk_mahasiswa INT NOT NULL,
+    sk_prodi INT NOT NULL,
+    sk_waktu INT NOT NULL,
+
+    jumlah_prestasi INT NOT NULL DEFAULT 1,
+
+    created_date DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY (sk_prestasi) REFERENCES dbo.Dim_Prestasi(sk_prestasi),
+    FOREIGN KEY (sk_mahasiswa) REFERENCES dbo.Dim_Mahasiswa(sk_mahasiswa),
+    FOREIGN KEY (sk_prodi) REFERENCES dbo.Dim_ProgramStudi(sk_prodi),
+    FOREIGN KEY (sk_waktu) REFERENCES dbo.Dim_Waktu(sk_waktu)
+);
+GO
+
+
+
+-- Fact Table: Fact_Anggaran
+CREATE TABLE dbo.Fact_Anggaran (
+    sk_anggaran INT NOT NULL,
+    sk_prodi INT NOT NULL,
+    sk_waktu INT NOT NULL,
+
+    total_anggaran DECIMAL(18,2) NOT NULL,
+
+    created_date DATETIME DEFAULT GETDATE(),
+FOREIGN KEY (sk_anggaran) REFERENCES dbo.Dim_Anggaran(sk_anggaran),
+    FOREIGN KEY (sk_prodi) REFERENCES dbo.Dim_ProgramStudi(sk_prodi),
+    FOREIGN KEY (sk_waktu) REFERENCES dbo.Dim_Waktu(sk_waktu)
+);
+GO
+
+
+
+-- Fact Table: Fact_Akreditasi
+CREATE TABLE dbo.Fact_Akreditasi (
+    sk_akreditasi INT NOT NULL,
+    sk_prodi INT NOT NULL,
+    sk_waktu INT NOT NULL,
+
+    jumlah_akreditasi INT NOT NULL DEFAULT 1,
+
+    created_date DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY (sk_akreditasi) REFERENCES dbo.Dim_Akreditasi(sk_akreditasi),
+    FOREIGN KEY (sk_prodi) REFERENCES dbo.Dim_ProgramStudi(sk_prodi),
+    FOREIGN KEY (sk_waktu) REFERENCES dbo.Dim_Waktu(sk_waktu)
+);
+GO
+
+– Fact Table: Fact_Akademik
+CREATE TABLE dbo.Fact_Akademik (
+    sk_prodi INT NOT NULL,
+    sk_waktu INT NOT NULL,
+
+    jumlah_mahasiswa_baru INT,
+    rata_rata_ipk DECIMAL(4,2),
+
+    created_date DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY (sk_prodi) REFERENCES dbo.Dim_ProgramStudi(sk_prodi),
+    FOREIGN KEY (sk_waktu) REFERENCES dbo.Dim_Waktu(sk_waktu)
+);
+GO
+
+– Fact Table: Fact_Dosen
+CREATE TABLE dbo.Fact_Dosen (
+    sk_dosen INT NOT NULL,
+    sk_prodi INT NOT NULL,
+    sk_waktu INT NOT NULL,
+
+    jumlah_dosen INT,
+    jumlah_mahasiswa INT,
+    rasio_dosen_mahasiswa DECIMAL(6,3),
+
+    created_date DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY (sk_dosen) REFERENCES dbo.Dim_Dosen(sk_dosen),
+    FOREIGN KEY (sk_prodi) REFERENCES dbo.Dim_ProgramStudi(sk_prodi),
+    FOREIGN KEY (sk_waktu) REFERENCES dbo.Dim_Waktu(sk_waktu)
+);
+GO
