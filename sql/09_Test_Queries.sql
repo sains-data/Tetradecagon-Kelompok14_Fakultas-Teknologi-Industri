c77de31bdc124173bcbdfb9a33c92080b30eb99e
Nabyla Sharfina <nabylasharfina8@gmail.com>
2025-11-24 21:36:13 +0700

    Update 09_Test_Queries.sql

--- sql/09_Test_Queries.sql
+++ sql/09_Test_Queries.sql
@@ -1,45 +1,69 @@
-CREATE TABLE etl.DataQualityAudit (
-    audit_id INT IDENTITY PRIMARY KEY,
-    check_name VARCHAR(200),
-    table_name VARCHAR(200),
-    issue_count INT,
-    check_timestamp DATETIME DEFAULT GETDATE()
-);
-GO
+-- Simple Aggregation Tests
+SELECT 
+    w.tahun, 
+    SUM(fp.jumlah_prestasi) AS total_prestasi
+FROM Fact_Prestasi fp
+JOIN Dim_Waktu w 
+    ON fp.sk_waktu = w.sk_waktu
+GROUP BY w.tahun
+ORDER BY w.tahun;
 
-CREATE PROCEDURE etl.Run_DataQualityChecks
-AS
-BEGIN
-    SET NOCOUNT ON;
+SELECT 
+    dp.nama_prodi, 
+    SUM(fa.total_anggaran) AS total_anggaran
+FROM Fact_Anggaran fa
+JOIN Dim_ProgramStudi dp 
+    ON dp.sk_prodi = fa.sk_prodi
+GROUP BY dp.nama_prodi
+ORDER BY dp.nama_prodi;
 
-    INSERT INTO etl.DataQualityAudit (check_name, table_name, issue_count)
-    SELECT 
-        'Missing NIM', 'Dim_Mahasiswa',
-        (SELECT COUNT(*) FROM dbo.Dim_Mahasiswa WHERE nim IS NULL OR nim = '');
+-- Complex Join Tests
+SELECT 
+    dm.nama_mahasiswa,
+    dp.nama_prodi,
+    w.tahun,
+    COUNT(fp.sk_prestasi) AS jumlah_prestasi
+FROM Fact_Prestasi fp
+JOIN Dim_Mahasiswa dm ON fp.sk_mahasiswa = dm.sk_mahasiswa
+JOIN Dim_ProgramStudi dp ON fp.sk_prodi = dp.sk_prodi
+JOIN Dim_Waktu w ON fp.sk_waktu = w.sk_waktu
+GROUP BY 
+    dm.nama_mahasiswa, 
+    dp.nama_prodi, 
+    w.tahun
+ORDER BY 
+    dm.nama_mahasiswa, 
+    w.tahun;
 
-    INSERT INTO etl.DataQualityAudit (check_name, table_name, issue_count)
-    SELECT 
-        'Duplicate NIM', 'Dim_Mahasiswa',
-        COUNT(*)
-    FROM (
-        SELECT nim FROM dbo.Dim_Mahasiswa GROUP BY nim HAVING COUNT(*) > 1
-    ) x;
+-- Drill-down Analysis
+SELECT 
+    dp.nama_prodi,
+    w.tahun,
+    w.semester,
+    AVG(fak.rata_rata_ipk) AS ipk_rata_rata
+FROM Fact_Akademik fak
+JOIN Dim_ProgramStudi dp ON dp.sk_prodi = fak.sk_prodi
+JOIN Dim_Waktu w ON w.sk_waktu = fak.sk_waktu
+GROUP BY 
+    dp.nama_prodi, 
+    w.tahun, 
+    w.semester
+ORDER BY 
+    dp.nama_prodi, 
+    w.tahun, 
+    w.semester;
 
-    INSERT INTO etl.DataQualityAudit (check_name, table_name, issue_count)
-    SELECT 
-        'Orphan FactPrestasi-Mahasiswa', 'Fact_Prestasi',
-        COUNT(*)
-    FROM dbo.Fact_Prestasi fp
-    LEFT JOIN dbo.Dim_Mahasiswa dm ON fp.sk_mahasiswa = dm.sk_mahasiswa
-    WHERE dm.sk_mahasiswa IS NULL;
+-- Full Scan Report
+SELECT *
+FROM Fact_Anggaran
+ORDER BY sk_waktu, sk_prodi;
 
-END;
-GO
-
-IF EXISTS (
-   SELECT 1 FROM etl.DataQualityAudit
-   WHERE issue_count > 0
-)
-BEGIN
-    RAISERROR ('DATA QUALITY ISSUE DETECTED!', 16, 1);
-END
+-- Index Stress Test
+SELECT 
+    dp.nama_prodi, 
+    SUM(fd.jumlah_dosen) AS total_dosen
+FROM Fact_Dosen fd
+JOIN Dim_ProgramStudi dp 
+    ON dp.sk_prodi = fd.sk_prodi
+GROUP BY dp.nama_prodi
+ORDER BY dp.nama_prodi
